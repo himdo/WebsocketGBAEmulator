ba6315c934a4c71096ec5e4e5586efdb5880777f
Lennon <hitzemanl@yahoo.com>
2022-11-13 22:53:25 -0800

    server: added button support; client: added button support

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { w3cwebsocket as W3CWebSocket } from 'websocket';
+import { client, w3cwebsocket as W3CWebSocket } from 'websocket';
 import Canvas from './Components/Canvas';
 import { Component, useState } from 'react';
+import { Button, Grid } from '@mui/material';
 
 class App extends Component {
   state = {
     frameData: undefined,
-    attempts: 0
+    attempts: 0,
+    client: undefined
   }
 
   connect () {
     const client = new W3CWebSocket('ws://localhost:8080');
     client.onopen = () => {
       console.info('[open] Connection established');
+      this.setState({client: client, attempts: 0})
       // client.send(JSON.stringify({ type: 'HANDSHAKE True' }));
     }
   
@@ -30,6 +33,7 @@ class App extends Component {
     };
   
     client.onclose = (e) => {
+      this.setState({client: undefined})
       if (e.wasClean) {
         console.info(`[close] Connection closed cleanly, code=${e.code} reason=${e.reason}`);
       } else {
@@ -48,23 +52,29 @@ class App extends Component {
     this.connect();
   }
 
+  sendButtonPress(button) {
+    if (typeof this.state.client !== "undefined") {
+      let data = {
+        type: 'ButtonPress',
+        data: button
+      }
+      this.state.client.send(JSON.stringify(data))
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Canvas width="160" height="144" framedata={this.state.frameData}/>
+          <Canvas style={{width:"40%", height: "40%"}} width="160" height="144" framedata={this.state.frameData}/>
+          <Button onClick={() => this.sendButtonPress('UP')}>UP</Button>
+          <Button onClick={() => this.sendButtonPress('RIGHT')}>RIGHT</Button>
+          <Button onClick={() => this.sendButtonPress('DOWN')}>DOWN</Button>
+          <Button onClick={() => this.sendButtonPress('LEFT')}>LEFT</Button>
+          <Button onClick={() => this.sendButtonPress('B')}>B</Button>
+          <Button onClick={() => this.sendButtonPress('A')}>A</Button>
+          <Button onClick={() => this.sendButtonPress('Start')}>Start</Button>
+          <Button onClick={() => this.sendButtonPress('Select')}>Select</Button>
         </header>
       </div>
     );
